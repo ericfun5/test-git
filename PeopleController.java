6fca86686eab6e2c452b8e8329e17c8f7130540b
ericfun5 <ericfun5@yahoo.com.tw>
2022-05-01 23:01:29 +0800

    mc2222

--- PeopleController.java
+++ PeopleController.java
@@ -32,7 +32,7 @@ public class PeopleController {
 	@GetMapping
 	@Timed(value = "all.people.request.time", description = "Time spend to return all people info.")
 	public List<Person> getAll() {
-		StreamSupport.stream(personRepository.findAll().spliterator(), false).toList();
+		Streamssss111111111111111111Support.stream(personRepository.findAll().spliterator(), false).toList();
 
 		return StreamSupport.stream(personRepository.findAll().spliterator(), false).toList();
 	}
