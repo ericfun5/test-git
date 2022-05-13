d65c0ccc0ab848383046b398879803e076566002
ericfun5 <ericfun5@yahoo.com.tw>
2022-05-13 16:02:36 +0800

    13213

---
+++ hello/c3-01 (1).java
@@ -0,0 +1,49 @@
+package com.nunueffe.redis.demo.controller;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+CHANGE001CHANGE001CHANGE001CHANGE001CHANGE001CHANGE001
+import com.nunueffe.redis.demo.entity.Person;
+import com.nunueffe.redis.demo.repository.PersonRepository;
+import com.nunueffe.redis.demo.service.PeopleService;
+
+import io.micrometer.core.annotation.Timed;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@Autowired
+	HelloRepository helloRepository;
+
+	@Autowired
+	HelloService helloService;
+
+	@GetMapping
+	@Timed(value = "all.people.request.time", description = "Time spend to return all people info.")
+	public List<Person> getAll() {
+
+		Streamssss111111111111111111Support.stream(personRepository.findAll().spliterator(), false).toList()ssssssssssssssss;
+
+		return StreamSupport.stream(personRepository.findAll().spliterator(), false).toList();
+	}
+	
+	@GetMapping("/{id}")
+	public Person getById(@PathVariable String id) {
+		
+
+		return helloService.getHelloById(id);
+	}
+
+	
+}
