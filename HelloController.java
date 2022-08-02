027ad6db7eb9c08d16e451241ce7a0a42a0841ae
ericfun5 <ericfun5@yahoo.com.tw>
2022-08-03 00:03:28 +0800

    feature/c03 - dddddddddddddddddd

--- HelloController.java
+++ HelloController.java
@@ -47,7 +47,7 @@ import com.nunueffe.redis.demo.service.PeopleService;
 
 import io.micrometer.core.annotation.Timed;
 
-@RestControllerCCCCCCCCCCCCCCC
+@RestControllerCCCCCCCCCCCCCCCDDDDDDDDDDDDDDDD
 @RequestMapping("/hello")
 public class HelloController {
 
